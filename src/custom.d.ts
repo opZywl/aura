2350754fd511d4f131b32f471221a7badfe5c67f
Zywl <86253343+opZywl@users.noreply.github.com>
2025-02-23 20:41:37 -0300

    feat: custom.d.ts compatible .svg/.png/.jpg + icons imports

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,3 +1,11 @@
+declare module '*.png' {
+    const value: string;
+    export default value;
+}
+declare module '*.jpg' {
+    const value: string;
+    export default value;
+}
 declare module '*.svg' {
     const content: string;
     export default content;
