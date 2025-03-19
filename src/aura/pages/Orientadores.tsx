71daaf6b5f72ad55c6ec94aaa6fcd33cd0cbcf4f
Zywl <86253343+opZywl@users.noreply.github.com>
2025-03-19 20:04:10 -0300

    feat: orientadora

--- src/aura/pages/Orientadores.tsx
+++ src/aura/pages/Orientadores.tsx
@@ -4,7 +4,7 @@ const Orientadores: React.FC = () => {
     return (
         <div className="p-8">
             <h1 className="text-3xl font-bold mb-4">Orientadores</h1>
-            <p>Pagina pra meter os bgl assinado da velha</p>
+            <p>Dedicado para a orientadora</p>
         </div>
     );
 };
